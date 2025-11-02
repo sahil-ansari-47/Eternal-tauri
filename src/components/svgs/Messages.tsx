9871c00705de820719507f4fd92e660b0ba99d64
Sandarva Podder <sandarvapodder09032004@gmail.com>
2025-11-02 17:09:39 +0530

    demo commit

--- src/components/svgs/Messages.tsx
+++ src/components/svgs/Messages.tsx
@@ -129,7 +129,7 @@ const Messages = ({ active = true }: { active: boolean }) => {
       version="1.1"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
         strokeLinecap="round"
